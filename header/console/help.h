44d45359e6801aeff0f39208ecbd402ce4ebfd5f
AlexanderSYN <000777000casa@gmail.com>
2026-06-02 23:21:14 +0300

    start add help

---
+++ header/console/help.h
@@ -0,0 +1,18 @@
+//
+// Created by AlexanderSYN on 02.06.2026.
+//
+
+#ifndef SIMPLEMINICOMMANDER_FOR_HELP_H
+#define SIMPLEMINICOMMANDER_FOR_HELP_H
+
+namespace help {
+    void output_help_color();
+    void output_help_console();
+    void output_help_date();
+    void output_help_my_commands();
+    void output_help_text();
+    void output_help_filesystem();
+    void output_help_all();
+}
+
+#endif //SIMPLEMINICOMMANDER_FOR_HELP_H
